ea41c2a8fc09875098df1278a5949a12ebee9a51
Choudhry Daniyal <100271560+choudhrydaniyal@users.noreply.github.com>
2025-08-25 16:53:45 +0500

    Revert "Modified courseinfo app for exercises 2.1-2.5"
    This reverts commit 129030eacf5e054959669b419e11da2d836433d3.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,56 +1,67 @@
-import Course from "./components/Course";
-
-const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
-
+const Header = (props) => {
   return (
     <>
-      <h1>Web Development Curriculum</h1>
-      <Course courses={courses} />
+      <h1>{props.course.name}</h1>
     </>
+  );
+};
+
+const Content = (props) => {
+  console.log("Content props: ", props);
+  return (
+    <>
+      <p>
+        {props.course.parts[0].name} {props.course.parts[0].exercises}
+      </p>
+      <p>
+        {props.course.parts[1].name} {props.course.parts[1].exercises}
+      </p>
+      <p>
+        {props.course.parts[2].name} {props.course.parts[2].exercises}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  console.log("Total props: ", props);
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content course={course}></Content>
+      <Total course={course}></Total>
+    </div>
   );
 };
 
