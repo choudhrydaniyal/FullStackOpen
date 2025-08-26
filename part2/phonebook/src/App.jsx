497ec8c255ea7161e776ab0286d14968f230c284
Choudhry Daniyal <100271560+choudhrydaniyal@users.noreply.github.com>
2025-08-27 01:47:25 +0500

    Modified phonebook app for exercises 2.12-2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { use, useState, useEffect } from "react";
 import axios from "axios";
+import contactServices from "./services/contacts";
 
 const SearchFilter = (props) => {
   return (
@@ -38,13 +39,16 @@ const InputField = (props) => {
   );
 };
 
-const DisplayContacts = ({ filteredContacts }) => {
+const DisplayContacts = ({ filteredContacts, handleDelete }) => {
   return (
     <div>
       {filteredContacts.map((person) => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
+        <div key={person.id}>
+          <p>
+            {person.name} {person.number}
+          </p>
+          <button onClick={() => handleDelete(person)}>Delete</button>
+        </div>
       ))}
     </div>
   );
@@ -58,7 +62,7 @@ const App = () => {
 
   useEffect(() => {
     console.log("effect");
-    axios.get("http://localhost:3001/persons").then((response) => {
+    contactServices.getAll().then((response) => {
       console.log("promise fulfilled, data: ", response.data);
       setPersons(response.data);
     });
@@ -66,17 +70,37 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
-    const exists = persons.some((person) => person.name == newName);
-    if (exists) {
-      alert(`${newName} already added to the phone book.`);
+    const person = persons.find(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
+    if (person) {
+      const newPerson = { ...person, number: newNumber };
+      if (
+        window.confirm(
+          `${newName} already added to the phone book, replace the old number with the new one?`
+        )
+      ) {
+        contactServices.updateContact(newPerson).then((response) => {
+          console.log("Updated contact. Contact: ", response.data);
+          setPersons(
+            persons.map((p) => (p.id === person.id ? response.data : p))
+          );
+          setNewName("");
+          setNewNumber("");
+        });
+      }
     } else if (newName == "" || newNumber == "") {
       alert("name or number can't be empty");
     } else {
       const newPerson = { name: newName, number: newNumber };
 
-      setPersons(persons.concat(newPerson));
-      setNewName("");
-      setNewNumber("");
+      contactServices.create(newPerson).then((response) => {
+        console.log("Added contact successfully. Contact: ", response.data);
+
+        setPersons(persons.concat(response.data));
+        setNewName("");
+        setNewNumber("");
+      });
     }
   };
 
@@ -90,6 +114,15 @@ const App = () => {
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleDelete = (person) => {
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      contactServices.deleteContact(person.id).then((response) => {
+        console.log("deleted succesfully. Deleted contact: ", response.data);
+        setPersons(persons.filter((person) => person.id !== response.data.id));
+      });
+    }
   };
 
   const filteredContacts = persons.filter((person) =>
@@ -112,7 +145,10 @@ const App = () => {
         handleNewNumber={handleNewNumber}
       ></NewContactForm>
       <h2>Numbers</h2>
-      <DisplayContacts filteredContacts={filteredContacts}></DisplayContacts>
+      <DisplayContacts
+        filteredContacts={filteredContacts}
+        handleDelete={handleDelete}
+      ></DisplayContacts>
     </div>
   );
 };
---
+++ part2/phonebook/src/services/contacts.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => {
+  return axios.get(baseUrl);
+};
+
+const create = (newContact) => {
+  return axios.post(baseUrl, newContact);
+};
+
+const deleteContact = (id) => {
+  return axios.delete(`${baseUrl}/${id}`);
+};
+
+const updateContact = (changedContact) => {
+  return axios.put(`${baseUrl}/${changedContact.id}`, changedContact);
+};
+
+export default { getAll, create, deleteContact, updateContact };
