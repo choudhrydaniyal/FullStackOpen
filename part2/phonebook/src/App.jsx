6f09b7e3da3c716d11b906d3115b702812f898e8
Choudhry Daniyal <100271560+choudhrydaniyal@users.noreply.github.com>
2025-08-26 15:20:55 +0500

    Modified phonebook for exercise 2.11
    Introduced axios and local server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { use, useState } from "react";
+import { use, useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchFilter = (props) => {
   return (
@@ -50,15 +51,18 @@ const DisplayContacts = ({ filteredContacts }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled, data: ", response.data);
+      setPersons(response.data);
+    });
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
